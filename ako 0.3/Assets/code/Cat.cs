bb7b72bf041a1a5f3adb258fa20ef4f27961fc56
BToruch <235999@edu.p.lodz.pl>
2023-11-18 00:13:59 +0100

    obecna faza i jej funkcje

--- ako 0.3/Assets/code/Cat.cs
+++ ako 0.3/Assets/code/Cat.cs
@@ -6,16 +6,19 @@ public class Cat : MonoBehaviour
 {
 
     private int wakeCounter;
-    private static int[] phaseThreshold = {0, 10, 20, 35, 50};
+    private int phase;
+    private static int[] phaseThreshold = { 0, 10, 20, 35, 50 };
     private bool isLocked;
 
     public int WakeCounter { get => wakeCounter; set => wakeCounter = value; }
     public global::System.Boolean IsLocked { get => isLocked; set => isLocked = value; }
+    public global::System.Int32 Phase { get => phase; set => phase = value; }
 
     // Start is called before the first frame update
     void Start()
     {
         WakeCounter = 0;
+        Phase = 0;
         IsLocked = false;
     }
 
@@ -37,7 +40,18 @@ public class Cat : MonoBehaviour
         Debug.Log("Cat counter: " + WakeCounter);
     }
 
-    public void isCounterLocked() {
+    public void isCounterLocked()
+    {
         if (WakeCounter >= phaseThreshold[4]) IsLocked = true;
+        Debug.Log("Cat phase 4 lock is " + IsLocked);
+    }
+
+    public void phaseCheck()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            if (WakeCounter >= phaseThreshold[i]) phase = i;
+        }
+        Debug.Log("Cat phase: " + phase);
     }
 }
