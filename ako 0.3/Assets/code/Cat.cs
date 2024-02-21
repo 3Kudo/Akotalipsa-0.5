617ed2147255c8bdc912d805fc5bf2357504db60
BToruch <235999@edu.p.lodz.pl>
2024-02-21 20:33:12 +0100

    kolejne fixiki

--- ako 0.3/Assets/code/Cat.cs
+++ ako 0.3/Assets/code/Cat.cs
@@ -90,7 +90,7 @@ public class Cat : MonoBehaviour
         pionek = pionek.GetComponent<Player>().pionek[Random.Range(0, 4)];
         int move = Random.Range(minMove, maxMove);
         Debug.Log(move);
-        if (pionek.GetComponent<Move>().waitPointIndex < 1 || move == 0 || pionek.GetComponent<Move>().defence)
+        if (pionek.GetComponent<Move>().waitPointIndex < 1 || move == 0 || pionek.GetComponent<Move>().defence || pionek.GetComponent<Move>().GetFinish())
             return;
         Transform position = pionek.GetComponent<Move>().GetWaitpoint();
         if (move < 0)
